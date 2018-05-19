a5bc52db116a449ecb90261ece27a91248ace41b
thelazyindian <satyabrat.me@gmail.com>
2018-05-19 17:47:08 +0000

    Update sftp-push.sh

--- sftp-push.sh
+++ sftp-push.sh
@@ -1,12 +1,17 @@
 # 1st argument - rom directory
 # 2nd argument - device name
 # 3rd argument - file upload name
-sudo apt install -y sshpass
+# Ex:- . sftp-push.sh /home/user/cardinal-aosp tissot Cardinal-AOSP-5.5-OREO-tissot-OFFICIAL-20180426.zip
+
+#sudo apt install -y sshpass
+cd ~
+wget https://github.com/thelazyindian/Scripts/raw/master/sshpass
+chmod +x sshpass
 export SSHPASS= # Enter password here
 cd $1/out/target/product/$2
-sshpass -e sftp -oBatchMode=no -b - teamcardinal@frs.sourceforge.net << !
+~/sshpass -e sftp -oBatchMode=no -b - teamcardinal@frs.sourceforge.net << !
    cd /home/frs/project/cardinal-aosp/$2
    put $3
    bye
 !
-cd ~/
+
